e37f629a7bd35a18d6cd1e89219fb69e47679594
Martí Bosch <marti.bosch@epfl.ch>
2020-11-02 12:58:20 +0100

    final article, figures, notebooks (+linting)

--- lausanne_greening_scenarios/utils.py
+++ lausanne_greening_scenarios/utils.py
@@ -3,26 +3,93 @@ import itertools
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+APPROACHES = ['random', 'scatter', 'cluster']
 
-def plot_approach_pairwise_comparison(df,
-                                      x,
-                                      y,
-                                      hue,
-                                      approaches=None,
-                                      base_figsize=None,
-                                      **barplot_kws):
+
+def _get_approach_comparison_cols(approaches):
+    return [
+        f'{first_approach}-{second_approach}'
+        for first_approach, second_approach in itertools.combinations(
+            approaches, 2)
+    ]
+
+
+def get_comparison_df(df, y, approaches=None, sortby=None):
+    if approaches is None:
+        # approaches = df['interaction'].unique()
+        approaches = APPROACHES
+
+    if sortby is None:
+        sortby = ['change_prop']
+
+    df = df[df['change_prop'] < 1]
+
+    def _get_approach_df(approach):
+        return df[df['interaction'] == approach].sort_values(sortby)
+
+    approach_pairs = itertools.combinations(approaches, 2)
+    first_approach, second_approach = next(approach_pairs)
+    comparison_df = _get_approach_df(first_approach).drop('interaction',
+                                                          axis=1)
+    comparison_df[f'{first_approach}-{second_approach}'] = comparison_df[
+        y] - _get_approach_df(second_approach)[y].values
+    for first_approach, second_approach in approach_pairs:
+        comparison_df[
+            f'{first_approach}-{second_approach}'] = _get_approach_df(
+                first_approach)[y].values - _get_approach_df(
+                    second_approach)[y].values
+
+    return comparison_df
+
+
+def get_absolute_comparison_df(comparison_df,
+                               groupby,
+                               y=None,
+                               approaches=None,
+                               agg='mean'):
+    if approaches is None:
+        approaches = APPROACHES
+    cols = groupby + _get_approach_comparison_cols(approaches)
+    if y is not None:
+        cols = [y] + cols
+    return comparison_df[cols].groupby(groupby).agg(agg)
+
+
+def get_relative_comparison_df(comparison_df,
+                               groupby,
+                               y,
+                               approaches=None,
+                               by_group_totals=False):
+    if approaches is None:
+        approaches = APPROACHES
+    approach_comparison_cols = _get_approach_comparison_cols(approaches)
+    divisor = comparison_df[y]
+    if by_group_totals:
+        divisor = divisor.groupby(
+            comparison_df['change_prop']).transform('sum')
+    return comparison_df[[y] + approach_comparison_cols].div(
+        divisor, axis='rows').assign(
+            **{col: comparison_df[col]
+               for col in groupby}).groupby(groupby).mean().drop(y, axis=1)
+
+
+def plot_approach_comparison(comparison_df,
+                             x,
+                             hue,
+                             approaches=None,
+                             base_figsize=None,
+                             **barplot_kws):
 
     if approaches is None:
-        approaches = ['random', 'scatter', 'cluster']
-
-    sortby = ['change_prop']
-    if hue != 'change_prop':
-        sortby += [hue]
+        approaches = APPROACHES
 
     if base_figsize is None:
         figwidth, figheight = plt.rcParams['figure.figsize']
     else:
         figwidth, figheight = base_figsize
+
+    if barplot_kws is None:
+        barplot_kws = {}
 
     approach_pairs = list(itertools.combinations(approaches, 2))
     num_pairs = len(approach_pairs)
@@ -33,24 +100,15 @@ def plot_approach_pairwise_comparison(df,
                              sharex=True,
                              sharey=True)
 
-    # _lulc_df = lulc_df[lulc_df['change_prop'] < 1]
-
-    def _get_approach_df(approach):
-        return df[df['interaction'] == approach].sort_values(sortby)
-
     for (first_approach, second_approach), ax in zip(approach_pairs,
                                                      axes.flat):
-        comparison_df = _get_approach_df(first_approach)
-        comparison_df[y] -= _get_approach_df(second_approach)[y].values
         sns.barplot(x=x,
-                    y=y,
+                    y=f'{first_approach}-{second_approach}',
                     hue=hue,
                     data=comparison_df,
                     ax=ax,
                     **barplot_kws)
         ax.set_ylabel('$N_{%s} - N_{%s}$' % (first_approach, second_approach))
-        ax.set_xlabel('Prop. transformed pixels')
-        ax.get_legend().remove()
         ax.axhline(0, color='gray', linestyle='--')
 
     return fig
