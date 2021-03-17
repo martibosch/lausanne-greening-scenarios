f732d3e7583b304cc10ae2b670507b8074d51077
Martí Bosch <marti.bosch@epfl.ch>
2021-03-17 04:22:20 +0100

    import setuptools, find packages, __init__.py in scenarios module

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import find_packages, setup
+import setuptools
 
-setup(
+setuptools.setup(
     name='lausanne-greening-scenarios',
-    packages=['lausanne_greening_scenarios'],
+    packages=setuptools.find_packages(exclude=['docs', 'tests*']),
     version='0.1.0',
-    description='Evaluation of the impact of a set of greening scenarios on air temperature in Lausanne',
+    description=
+    'Evaluation of the impact of a set of greening scenarios on air temperature in Lausanne',
     author='Martí Bosch',
     license='GPL-3.0',
 )
